5369eba5fe7b0666ee177828600dc551350fd5c3
V3Solution <mubeenyasin75@gmail.com>
2023-10-20 13:07:59 +0500

    chaper-03 is completed

--- chapter-03/build/js/main.js
+++ chapter-03/build/js/main.js
@@ -20,3 +20,30 @@ myTuple[1] = 23;
 let myObj;
 myObj = [];
 console.log(typeof myObj);
+myObj = bands;
+console.log(myObj, "this is the object value");
+myObj = {};
+const exampleObj = {
+    prop1: "Muhammad Mubeen Yasin",
+    prop2: true,
+};
+exampleObj.prop1 = "Muhammad Mubeen";
+console.log(exampleObj, "this is the new object that we created");
+let evh = {
+    name: "Mubeen",
+    active: false,
+    albums: ["The Dark Side of the Moon", 1994],
+};
+const greetGuitarist = (guitarist) => {
+    return `Hello ${guitarist.name}!`;
+};
+console.log(greetGuitarist(evh));
+let obj2 = {
+    name: "M Mubeen",
+    active: true,
+    albums: ["The Dark Side of the Moon", 1994],
+    age: 30,
+};
+console.log(obj2, "this is the object that is created through the interface...");
+//Enums
+//unlike most typescript features, enums are not a type-level addition to javascript but something added to the language and runtime
--- chapter-03/src/main.ts
+++ chapter-03/src/main.ts
@@ -22,9 +22,64 @@ mixed = myTuple;
 
 myTuple[1] = 23;
 
-
 //Objects
 
-let myObj: object
-myObj = []
-console.log(typeof myObj)
+let myObj: object;
+myObj = [];
+console.log(typeof myObj);
+
+myObj = bands;
+console.log(myObj, "this is the object value");
+
+myObj = {};
+const exampleObj = {
+  prop1: "Muhammad Mubeen Yasin",
+  prop2: true,
+};
+
+exampleObj.prop1 = "Muhammad Mubeen";
+
+console.log(exampleObj, "this is the new object that we created");
+
+type Guitarist = {
+  name: string;
+  active?: boolean; // by adding the ? with the attribute we make that is optional...
+  albums: (string | number)[];
+};
+
+let evh: Guitarist = {
+  name: "Mubeen",
+  active: false,
+  albums: ["The Dark Side of the Moon", 1994],
+};
+
+const greetGuitarist = (guitarist: Guitarist) => {
+  return `Hello ${guitarist.name}!`;
+};
+
+console.log(greetGuitarist(evh));
+interface newTypeForTesting {
+  name: string;
+  active: boolean;
+  albums: (string | number)[];
+}
+
+interface newInterface2 extends newTypeForTesting {
+  age: number;
+}
+
+let obj2: newInterface2 = {
+  name: "M Mubeen",
+  active: true,
+  albums: ["The Dark Side of the Moon", 1994],
+  age: 30,
+};
+
+console.log(
+  obj2,
+  "this is the object that is created through the interface..."
+);
+
+
+//Enums
+//unlike most typescript features, enums are not a type-level addition to javascript but something added to the language and runtime
